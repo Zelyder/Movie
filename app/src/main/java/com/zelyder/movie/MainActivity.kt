806cb5341a2b14f75d83dfccb8a76d69ec638464
Zelyder <57kaa@mail.ru>
2020-12-02 20:21:27 +0300

    Removed the isFirstOpen key

--- app/src/main/java/com/zelyder/movie/MainActivity.kt
+++ app/src/main/java/com/zelyder/movie/MainActivity.kt
@@ -11,32 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(isFirstOpen){
+
+        if(savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.main_container, MoviesListFragment())
-                .addToBackStack("MoviesList")
                 .commit()
         }
 
-        isFirstOpen = false
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBoolean(KEY_FIRST_OPEN, isFirstOpen)
-
-
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        isFirstOpen = savedInstanceState.getBoolean(KEY_FIRST_OPEN)
-
-    }
-    companion object {
-        var isFirstOpen = true
-    }
-
 }
-
-private const val KEY_FIRST_OPEN = "isFirstOpen"
