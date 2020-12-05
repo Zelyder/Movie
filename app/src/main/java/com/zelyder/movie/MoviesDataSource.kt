1b0956c544465f21c9a0ef407074fccf16038a9d
Zelyder <57kaa@mail.ru>
2020-12-05 11:57:16 +0300

    Added data for details screen

--- app/src/main/java/com/zelyder/movie/MoviesDataSource.kt
+++ app/src/main/java/com/zelyder/movie/MoviesDataSource.kt
@@ -12,17 +12,17 @@ class MoviesDataSource {
             Movie(
                 1, "Tenet", R.drawable.tenet_card_poster,
                 "16+", true, "Action, Sci-Fi, Thriller", 5f,
-                98, 97, -1, "No data"
+                98, 97, R.drawable.tenet_big_poster, "Armed with only one word - Tenet - and fighting for the survival of the entire world, the Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time."
             ),
             Movie(
                 2, "Black Widow", R.drawable.black_widow_card_poster,
                 "13+", false, "Action, Adventure, Sci-Fi", 4f,
-                38, 102, -1, "No data"
+                38, 102, R.drawable.black_widow_big_poster, "Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy and the broken relationships left in her wake long before she became an Avenger."
             ),
             Movie(
                 3, "Wonder Woman 1984", R.drawable.wonder_woman_card_poster,
                 "13+", false, "Action, Adventure, Fantasy", 5f,
-                74, 120, -1, "No data"
+                74, 120, R.drawable.wonder_woman_big_poster, "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah."
             )
         )
     }
