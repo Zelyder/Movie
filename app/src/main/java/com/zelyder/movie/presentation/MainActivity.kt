8bb1bb0cf489eebdaf81a4f8eb89e74df9fd9267
Zelyder <57kaa@mail.ru>
2021-02-14 12:18:45 +0300

    Added deep link support

--- app/src/main/java/com/zelyder/movie/presentation/MainActivity.kt
+++ app/src/main/java/com/zelyder/movie/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.zelyder.movie.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.zelyder.movie.R
 import com.zelyder.movie.presentation.core.NavigationClickListener
 import com.zelyder.movie.presentation.moviedetails.MoviesDetailsFragment
@@ -19,8 +21,17 @@ class MainActivity : AppCompatActivity(), NavigationClickListener {
             supportFragmentManager.beginTransaction()
                 .add(R.id.main_container, MoviesListFragment())
                 .commit()
+
+            intent?.let(::handleIntent)
         }
 
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            handleIntent(intent)
+        }
     }
 
     override fun onClickBack() {
@@ -28,9 +39,21 @@ class MainActivity : AppCompatActivity(), NavigationClickListener {
     }
 
     override fun navigateToDetails(id: Int) {
+        Log.d("LOL", "id = $id")
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_container, MoviesDetailsFragment.newInstance(id))
             .addToBackStack("MoviesDetails")
             .commit()
+    }
+
+    private fun handleIntent(intent: Intent) {
+        when(intent.action) {
+            Intent.ACTION_VIEW -> {
+                val id = intent.data?.lastPathSegment?.toIntOrNull()
+                if (id != null) {
+                    navigateToDetails(id)
+                }
+            }
+        }
     }
 }
