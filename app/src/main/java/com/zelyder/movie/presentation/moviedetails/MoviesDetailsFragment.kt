83fe3b42f7731ffb49fdfe4371e21d0be551b362
Zelyder <57kaa@mail.ru>
2021-02-15 12:23:18 +0300

    Added date and time selection dialogs. Added event recording to the calendar.

--- app/src/main/java/com/zelyder/movie/presentation/moviedetails/MoviesDetailsFragment.kt
+++ app/src/main/java/com/zelyder/movie/presentation/moviedetails/MoviesDetailsFragment.kt
@@ -4,12 +4,14 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
-import android.content.pm.LauncherActivityInfo
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
@@ -21,14 +23,16 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
-import com.zelyder.movie.presentation.core.BaseFragment
-import com.zelyder.movie.presentation.core.NavigationClickListener
 import com.zelyder.movie.R
 import com.zelyder.movie.domain.models.DetailsMovie
+import com.zelyder.movie.presentation.core.BaseFragment
+import com.zelyder.movie.presentation.core.NavigationClickListener
 import com.zelyder.movie.viewModelFactoryProvider
+import java.util.*
 
 
-class MoviesDetailsFragment : BaseFragment() {
+class MoviesDetailsFragment : BaseFragment(), DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
 
     var navigationClickListener: NavigationClickListener? = null
     var movieDetailsClickListener: MovieDetailsClickListener? = null
@@ -148,9 +152,6 @@ class MoviesDetailsFragment : BaseFragment() {
         }
     }
 
-    private fun onCalendarPermissionGranted() {
-        Toast.makeText(context, "onCalendarPermissionGranted", Toast.LENGTH_SHORT).show()
-    }
 
     private fun showCalendarPermissionExplanationDialog() {
         AlertDialog.Builder(requireContext())
@@ -188,6 +189,86 @@ class MoviesDetailsFragment : BaseFragment() {
         requestPermissionLauncher.launch(Manifest.permission.WRITE_CALENDAR)
     }
 
+
+    private var day = 0
+    private var month = 0
+    private var year = 0
+    private var hour = 0
+    private var minute = 0
+
+    private var savedDay = 0
+    private var savedMonth = 0
+    private var savedYear = 0
+    private var savedHour = 0
+    private var savedMinute = 0
+
+    private fun onCalendarPermissionGranted() {
+        openDateTimePicker()
+    }
+
+    private fun getDateTimeCalendar() {
+        val calendar = Calendar.getInstance()
+        day = calendar.get(Calendar.DAY_OF_MONTH)
+        month = calendar.get(Calendar.MONTH)
+        year = calendar.get(Calendar.YEAR)
+        hour = calendar.get(Calendar.HOUR)
+        minute = calendar.get(Calendar.MINUTE)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedDay = dayOfMonth
+        savedMonth = month
+        savedYear = year
+
+        getDateTimeCalendar()
+        TimePickerDialog(requireContext(), this, hour, minute, true).show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+
+        writeToCalendar(
+            tvTitle.text.toString(),
+            savedYear,
+            savedMonth,
+            savedDay,
+            savedHour,
+            savedMinute
+        )
+    }
+
+    private fun openDateTimePicker() {
+        getDateTimeCalendar()
+
+        DatePickerDialog(requireContext(), this, year, month, day).show()
+    }
+
+    private fun writeToCalendar(
+        movieTitle: String,
+        year: Int,
+        month: Int,
+        dayOfMonth: Int,
+        hourOfDay: Int,
+        minute: Int
+    ) {
+        val startMillis: Long = Calendar.getInstance().run {
+            set(year, month, dayOfMonth, hourOfDay, minute)
+            timeInMillis
+        }
+        val endMillis: Long = Calendar.getInstance().run {
+            set(year, month, dayOfMonth, hourOfDay + 2, minute)
+            timeInMillis
+        }
+
+        val intent = Intent(Intent.ACTION_INSERT)
+            .setData(CalendarContract.Events.CONTENT_URI)
+            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startMillis)
+            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endMillis)
+            .putExtra(CalendarContract.Events.TITLE, requireContext().resources.getString(R.string.reminder_title))
+            .putExtra(CalendarContract.Events.DESCRIPTION, requireContext().resources.getString(R.string.reminder_description, movieTitle))
+        startActivity(intent)
+    }
 
 
     companion object {
